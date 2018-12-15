fdec839fd606a3b523248c380aac7803ee859372
Jinli Yu <yujl4194@brandeis.edu>
2018-12-14 22:02:02 -0500

    add comment

--- web_app/crawler/crawler.py
+++ web_app/crawler/crawler.py
@@ -1,5 +1,6 @@
 """
 jinli yu
+chromedriver is required
 e.g. python crawler.py harry\ potter
 """
 from urllib.parse import quote
@@ -9,13 +10,13 @@ import sys
 
 def get_review_html(movie_name):
     try:
-        search_url = "https://www.rottentomatoes.com/search/?search="+quote(movie_name)
+        search_url = "https://www.rottentomatoes.com/search/?search="+quote(movie_name) #url to search by movie name
         option = webdriver.ChromeOptions()
         option.add_argument('headless')
         driver = webdriver.Chrome(chrome_options=option)
         driver.get(search_url)
-        driver.find_element_by_xpath("//section[@id='movieSection']//a[1]").click()
-        driver.find_element_by_xpath("//section[@id='audience_reviews']//div[@class='view-all']/a").click()
+        driver.find_element_by_xpath("//section[@id='movieSection']//a[1]").click() #first movie in the list
+        driver.find_element_by_xpath("//section[@id='audience_reviews']//div[@class='view-all']/a").click()#audiance reviews
         html = driver.page_source
     except Exception as e:
         print("Cannot find this movie or there is no audience reviews for this movie now!")
@@ -41,6 +42,7 @@ if __name__ == "__main__":
     if(len(html)==0):
         sys.exit()
     reviews = get_reviews(html)
+    #write reviews to file
     fw = open("./reviews for "+movie_name,"w")
     for review in reviews:
         fw.write(str(review))
