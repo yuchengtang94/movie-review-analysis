589caa037757727b87fb9b82e1eca7236e14c74c
yuchengtang94 <yuchengtang94@gmail.com>
2018-12-19 15:48:40 -0500

    requirements

--- build_env.sh
+++ build_env.sh
@@ -5,10 +5,4 @@
 # @contact:   yichuanxiong@brandeis.edu
 # @version:   $Id$
 brew cask install chromedriver
-pip install joblib
-pip install flask==1.0.2
-pip install flask-wtf==0.14.2
-pip install bootstrap-flask==1.0.9
-pip install selenium==3.141.0
-pip install BeautifulSoup4
-pip install keras
+pip install -r requirements.txt
