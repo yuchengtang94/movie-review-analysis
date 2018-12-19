aeeeadecf38008f12573f394e59d8d04c9164007
易川雄 <yichuanxiong@corp.netease.com>
2018-12-19 01:56:03 -0500

    build env bash

---
+++ build_env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# FileName:   build_env.sh
+# Author:     Chuanxiong Yi
+# @contact:   yichuanxiong@brandeis.edu
+# @version:   $Id$
+brew cask install chromedriver
+pip install joblib
+pip install flask==1.0.2
+pip install flask-wtf==0.14.2
+pip install bootstrap-flask==1.0.9
+pip install selenium==3.141.0
+pip install BeautifulSoup4
